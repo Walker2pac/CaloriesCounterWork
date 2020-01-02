94d6356207ff65c273cc29d4f8fb7f989cf5d2d3
Emirlan <emka.aidaov@yandex.ru>
2020-01-02 11:54:43 +0600

    added etwas

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -6,4 +6,16 @@ public class GameConstants : MonoBehaviour
 {
     public const string KEY_DIET_NAME = "DietName";
     public const string KEY_INTENSITY_NAME = "IntensityName";
+    public const string DIET_NAME_STANDART = "Стандартная";
+    public const string DIET_NAME_CLASSIC = "Классическая(снижение веса -10%)";
+    public const string DIET_NAME_ZONES = "Диета Зона";
+    public const string DIET_NAME_ATKINS = "Диета Аткинса";
+    public const string DIET_NAME_HIGH_CARB = "Высокоуглеводная";
+    public const string DIET_NAME_KETOGENIC = "Кетогенная";
+    
+    public const string INTENSITY_NAME_SEDENTARY_WORK = "Сидячая работа";
+    public const string INTENSITY_NAME_2_TIMES_TRAINING = "Тренировка 1-2 раз в неделю";
+    public const string INTENSITY_NAME_4_TIMES_TRAINING = "Тренировка 2-4 раз в неделю";
+    public const string INTENSITY_NAME_6_TIMES_TRAINING = "Тренировка 4-6 раз в неделю";
+    public const string INTENSITY_NAME_EVERYDAY = "Каждый день";
 }
