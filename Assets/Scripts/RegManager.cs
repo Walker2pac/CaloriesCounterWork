442d4f2767efe2d35e4d0f43d9b39686618669b0
Emirlan <emka.aidaov@yandex.ru>
2020-01-02 11:12:22 +0600

    created stage 3-4

--- Assets/Scripts/RegManager.cs
+++ Assets/Scripts/RegManager.cs
@@ -27,6 +27,7 @@ public class RegManager : MonoBehaviour
     [SerializeField] private GameObject stage1;
     [SerializeField] private GameObject stage2;
     [SerializeField] private GameObject stage3;
+    [SerializeField] private GameObject stage4;
     [SerializeField] private TMP_InputField inputWeight;
     [SerializeField] private TMP_InputField inputWeight2;
     [SerializeField] private TMP_InputField inputHeight;
@@ -46,13 +47,13 @@ public class RegManager : MonoBehaviour
     {
         ActivateStage1();
     }
-    
     public void ActivateStage1()
     {
-        stage2.SetActive(false);
         stage1.SetActive(true);
+        stage2.SetActive(false);
+        stage3.SetActive(false);
+        stage4.SetActive(false);
     }
-    
     public void ActivateStage2()
     {
         if (!debug)
@@ -65,29 +66,40 @@ public class RegManager : MonoBehaviour
             {
                 stage1.SetActive(false);
                 stage2.SetActive(true);
+                stage3.SetActive(false);
+                stage4.SetActive(false);
             }
         }
         else
         {
             stage1.SetActive(false);
             stage2.SetActive(true);
+            stage3.SetActive(false);
+            stage4.SetActive(false);
         }
     }
-
-    public void ActivateStage3()
-    {
-        stage1.SetActive(false);
-        stage2.SetActive(false);
-        stage3.SetActive(true);
-    }
-    
     public void ActivateStage2f()
     {
         stage1.SetActive(false);
         stage2.SetActive(true);
         stage3.SetActive(false);
+        stage4.SetActive(false);
     }
-    
+    public void ActivateStage3()
+    {
+        stage1.SetActive(false);
+        stage2.SetActive(false);
+        stage3.SetActive(true);
+        stage4.SetActive(false);
+    }
+
+    public void ActivateStage4()
+    {
+        stage1.SetActive(false);
+        stage2.SetActive(false);
+        stage3.SetActive(false);
+        stage4.SetActive(true);
+    }
     public void ActiveIntensity()
     {
         panelIntensity.SetActive(true);
@@ -98,10 +110,8 @@ public class RegManager : MonoBehaviour
         intensityButton.SetActive(false);
         dietButton.SetActive(false);
     }
-    
     public void ActiveDiet()
     {
-        
         panelDiet.SetActive(true);
         closeInfoButton.SetActive(true);
         panelQuestion.SetActive(true);
@@ -110,7 +120,6 @@ public class RegManager : MonoBehaviour
         intensityButton.SetActive(false);
         dietButton.SetActive(false);
     }
-
     public void CloseInfoPanel()
     {
         closeInfoButton.SetActive(false);
@@ -122,18 +131,14 @@ public class RegManager : MonoBehaviour
         dietQuestion.SetActive(false);
         intensityButton.SetActive(true);
         dietButton.SetActive(true);
-    } 
-    
+    }
     public void CloseAttention()
     {
         warningPanel.SetActive(false);
     }
-
     public GameObject GetDietDescriptionPanel()
     {
         return dietDescriptionPanel;
     }
-
-    
 }
 
